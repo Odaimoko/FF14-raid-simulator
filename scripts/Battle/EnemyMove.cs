1d9f5264ed10a26be65a42edada57aeb37041389
oda <odaimoko@gmail.com>
2020-09-16 10:44:17 +0800

    initial commit

---
+++ scripts/Battle/EnemyMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    // The same for both Player and Enemy?
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
